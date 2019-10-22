d8bad1d31937c26a3690cdecec427d35b6833a79
yuritakeo <37599776+yuritakeo@users.noreply.github.com>
2019-10-22 15:18:27 -0300

    Tela Menu - Design

--- src/conferencia/gps/ConferenciaGps.java
+++ src/conferencia/gps/ConferenciaGps.java
@@ -7,8 +7,8 @@ package conferencia.gps;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 /**
@@ -16,15 +16,19 @@ import javafx.stage.Stage;
  * @author yuri.miyazaki
  */
 public class ConferenciaGps extends Application {
-    
+
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+        ScrollPane scrollPane = loader.load();
+
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene mainScene = new Scene(scrollPane);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Conferencia GPS");
+        primaryStage.show();
     }
 
     /**
@@ -33,5 +37,5 @@ public class ConferenciaGps extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
